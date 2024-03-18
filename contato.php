de2f77e48166081ddb5ba681d658481632b40b50
moacirmoraes <moacir.rodrigs@gmail.com>
2024-03-18 00:57:07 -0300

    Alerta customizado

--- contato.php
+++ contato.php
@@ -1,21 +1,41 @@
 <?php
+    libxml_use_internal_errors(true);
+    $dom = new \DOMDocument();
+    $dom->loadHTMLFile("./index.html");
 
     $nome = addslashes($_POST['nome']);
     $email = addslashes($_POST['email']);
     $telefone = addslashes($_POST['telefone']);
     $mensagem = addslashes($_POST['mensagem']);
 
-    $to = "contato@emoretech.com.br";
+    // $to = "contato@emoretech.com.br";
+    $to = "moacir.rodrigs@gmail.com";
+
     $subject = "Contato $nome";
 
     $body = "$mensagem \n Telefone: $telefone";
 
     $header = "From: $to \n Reply-to: $email \n X=Mailer:PHP/".phpversion();
 
+    $message = "";
+
     if(mail($to, $subject, $body, $header)){
-        echo("Email enviado!");
+        $message = "Email enviado!";
     }
     else{
-        echo("Erro ao enviar e-mail");
+        $message = "Erro ao enviar e-mail";
     }
+   
+    $alertElement = $dom->getElementById('alert');
+
+    if ($alertElement) {
+        $alertMessage = $dom->createElement("p", $message);
+
+        $alertElement->setAttribute('class', 'alert open');
+        $alertElement->appendChild($alertMessage);        
+    } else {        
+        echo "Elemento #alert não encontrado.";
+    }
+
+    echo $dom->saveHTML();
 ?>
--- index.js
+++ index.js
@@ -2,6 +2,9 @@ let btnMenu = document.getElementById('btn-abrir-menu');
 let menu = document.getElementById('menu-mobile');
 let overlay = document.getElementById('overlay-menu');
 
+let closeAlert = document.getElementById("close-alert");
+let customAlert = document.getElementById("alert");
+
 btnMenu.addEventListener('click', ()=> {
     menu.classList.add('abrir-menu');
 })
@@ -18,4 +21,8 @@ window.addEventListener('scroll', ()=>{
     let scroll = document.querySelector('.btn-scroll-top a');
 
     scroll.classList.toggle('active', window.scrollY > 450);
+})
+
+closeAlert.addEventListener('click', ()=>{
+    customAlert.classList.remove('open');
 })
